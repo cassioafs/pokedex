2a9e2db5584aea649881200a8395203ee5df7af1
Leonardo Almeida <leonardo.almeida@concrete.com.br>
2017-09-25 14:43:35 -0300

    adicionando validação e fotos random

--- components/pokemons/pokemons.controller.js
+++ components/pokemons/pokemons.controller.js
@@ -3,10 +3,30 @@
 
     function PokemonsController (Pokemons){
         vm = this;
-
+        var imagesArray = ['/img/026.png',
+        '/img/031.png', 
+        '/img/031.png', 
+        '/img/031.png', 
+        '/img/032.png', 
+        '/img/034.png', 
+        '/img/059.png', 
+        '/img/064.png', 
+        '/img/071.png',
+        '/img/012.png']
         vm.pokemons = Pokemons.getAll();
         vm.save = function (){
+            vm.pokemon.image = imagesArray[Math.floor(Math.random() * 10) + 1];
             Pokemons.pokemons.push(vm.pokemon);
+            vm.pokemon = {
+                id: '',
+                name: '',
+                type1: 'grama',
+                type2: 'grama',
+                attack: '',
+                defense: '',
+                hp: '',
+                image : ''
+            };
             vm.pokemons = Pokemons.getAll();
             $('#myModal').modal('hide');
         }
